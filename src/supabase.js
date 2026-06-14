0313f108ce8a41290f4e13d05aa3c6a09a1aa0bc
Hamza Bahbah <hamzabahbah@MacBook-Pro-de-Hamza.local>
2026-06-14 21:42:01 +0400

    fix supabase url

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://my-tasks-weld.vercel.app/'
-const SUPABASE_KEY = '6PD4NIvTRyz3uKWz'
+const SUPABASE_URL = 'https://kezoejzvljpollddddsv.supabase.co/rest/v1/'
+const SUPABASE_KEY = 'sb_publishable_JoophiA_j6v7j8Tcq3bQGg_A6_toMOl'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
